c1aa62df0dbfa2c39a6c6c6cbcf2b050d7a00d76
Anup minj <anupminz04@gmail.com>
2026-04-22 09:07:53 +0530

    fix: removed incorrect symbols from vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev
 export default defineConfig({
-  // ✅ Sahi format sirf repository ka naam hota hai
   base: "/://online-game-stores.com", 
   plugins: [react()],
 })
