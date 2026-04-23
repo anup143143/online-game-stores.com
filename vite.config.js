46ab1e9f8323f5af4d231358f167eff19ce4c96f
Anup minj <anupminz04@gmail.com>
2026-04-23 08:32:02 +0530

    fix: final base path correction

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
   base: "/://online-game-stores.com", 
